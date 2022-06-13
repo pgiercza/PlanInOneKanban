99955ef18c8ba5c9bf106a50640bd462b6dfb221
Piotrek <pgiercza@gmail.com>
2022-06-13 02:01:02 +0200

    Initial commit Project Topic

---
+++ src/test/java/pl/edu/wszib/planinonekanban/PlanInOneKanbanApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.wszib.planinonekanban;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanInOneKanbanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
