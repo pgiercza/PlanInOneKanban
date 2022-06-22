f5ef105940d8e850d97948f6fc62167bbb41485b
Piotrek <pgiercza@gmail.com>
2022-06-22 15:47:02 +0200

    Frontend Commit 1

---
+++ src/main/java/pl/edu/wszib/planinonekanban/web/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package pl.edu.wszib.planinonekanban.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+    @Controller
+    public class HomeController {
+
+        @GetMapping()
+        public String showHomePage(Model model) {
+            model.addAttribute("name", "X");
+            return "homePage";
+        }
+    }
+
