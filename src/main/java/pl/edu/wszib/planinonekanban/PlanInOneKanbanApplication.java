d2292192bfac1534409beb7abbc07080dafb1d70
Piotrek <pgiercza@gmail.com>
2022-07-23 00:57:44 +0200

    Some updates

--- src/main/java/pl/edu/wszib/planinonekanban/PlanInOneKanbanApplication.java
+++ src/main/java/pl/edu/wszib/planinonekanban/PlanInOneKanbanApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:db.properties")
+//@PropertySource("classpath:db.properties")
 public class PlanInOneKanbanApplication {
 
     public static void main(String[] args) {
